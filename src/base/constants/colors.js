d8d70f2d67dd8efb77f5e34229f570eb58e63627
Hwajiin <borderline0px@gmail.com>
2021-11-26 20:43:00 +0900

    💄 Set constants about colors

---
+++ src/base/constants/colors.js
@@ -0,0 +1,23 @@
+const colors = {
+  variable: {
+    // Primary color
+    blue: "#2962ff",
+    "blue-light": "#768fff",
+    "blue-dark": "#0039cb",
+
+    // Secondary color
+    gray: "#eceff1",
+    "gray-dark": "#babdbe",
+
+    // Point color
+    red: "#ff1744",
+    "red-light": "#ff616f",
+    "red-dark": "#c4001d",
+
+    // Base color
+    white: "#ffffff",
+    black: "000",
+  },
+};
+
+export default colors;
