f1952b15d605990868b945eb44166b4048f081be
Hwajiin <borderline0px@gmail.com>
2021-11-26 20:41:57 +0900

    💄 Set globalStyle with importing styled-normalize

---
+++ src/base/globalStyle.js
@@ -0,0 +1,67 @@
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
+import colors from "./constants/colors";
+import fonts from "./constants/fonts";
+
+const GlobalStyle = createGlobalStyle`
+    ${normalize};
+    
+    /* Additional Customizing reset CSS */
+    * {
+        margin: 0;
+        font-family: ${fonts.family};
+        box-sizing: border-box;
+    }
+
+    html {
+        font-family: ${fonts.family};
+        font-size: 16px;
+    }
+
+    body {
+        font-family: ${fonts.family};
+        color: ${colors.variable.black};
+    }
+
+    h1 {
+        margin: 0;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+
+    button,
+    input,
+    select,
+    textarea {
+        background-color: transparent;
+        border: 0;
+    }
+
+    button:focus,
+    input:focus,
+    select:focus,
+    textarea:focus {
+        outline: none;
+        box-shadow: none;
+    }
+
+    a,
+    button {
+        cursor: pointer;
+    }
+
+    button {
+        padding: 0;
+    }
+
+    ul,
+    ol {
+        padding-left: 0;
+        list-style: none;
+    }
+    `;
+
+export default GlobalStyle;
