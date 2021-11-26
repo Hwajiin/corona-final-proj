9091ad314449308fecf45c3c265696801845aea2
Hwajiin <borderline0px@gmail.com>
2021-11-26 20:49:49 +0900

    🎨 Implement flexbox function for alignment

---
+++ src/base/mixins/flexbox.ts
@@ -0,0 +1,29 @@
+type Justify =
+  | "center"
+  | "flex-start"
+  | "flex-end"
+  | "stretch"
+  | "space-around"
+  | "space-between"
+  | "space-evenly";
+
+type Align = "center" | "flex-start" | "flex-end" | "stretch" | "baseline";
+
+type FlexDirection = "row" | "column";
+
+// flexbox 커스텀하는 함수
+
+const flexbox = (
+  justify: Justify = "center",
+  align: Align = "center",
+  direction: FlexDirection = "row"
+) => {
+  return `
+        display: flex;
+        flex-direction: ${direction};
+        justify-contents: ${justify};
+        align-items: ${align};
+    `;
+};
+
+export default flexbox;
