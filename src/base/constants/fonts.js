dc1802d84ddd5837c619367d0bc617f0d32ba86d
Hwajiin <borderline0px@gmail.com>
2021-11-26 20:43:12 +0900

    💄 Set constants about fonts

---
+++ src/base/constants/fonts.js
@@ -0,0 +1,20 @@
+const fonts = {
+  family: {
+    base: `'Nanum Gothic', sans-serif`,
+  },
+
+  size: {
+    sm: "12px",
+    md: "14px",
+    lg: "18px",
+    xl: "24px",
+  },
+
+  weight: {
+    light: 400,
+    bold: 700,
+    "extra-bold": 800,
+  },
+};
+
+export default fonts;
