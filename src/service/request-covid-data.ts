112f76c0459a31d3e8a2414195745585450116e1
Hwajiin <borderline0px@gmail.com>
2021-11-29 23:28:29 +0900

    ✨ Create openApi axios instance

---
+++ src/service/request-covid-data.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import generateDate from "./generateDate";
+
+const date = generateDate();
+
+export interface AxiosConfig {
+  baseURL: string;
+  params: object;
+  headers?: any;
+}
+
+// covid19 시·도발생 및 연령별·성별감염 현황에 관한 axios instance
+const openApiConfig: AxiosConfig = {
+  baseURL: "http://openapi.data.go.kr/openapi/service/rest/Covid19",
+  params: {
+    serviceKey: process.env.REACT_APP_OPEN_API_KEY,
+    pageNo: 1,
+    numOfRows: 10,
+    startCreateDt: date,
+    endCreateDt: date,
+  },
+};
+
+const openApiAxiosInstance = axios.create(openApiConfig);
+
+export default openApiAxiosInstance;
