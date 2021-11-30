446230a32bc89faa07a5598b608a0a5818ef49ad
Hwajiin <borderline0px@gmail.com>
2021-11-30 10:27:07 +0900

    ✨ Create getCurrentGeolocation function

---
+++ src/service/get-current-pos.ts
@@ -0,0 +1,14 @@
+// 현재 사용자 위치를 가져오는 함수
+export type GeoSuccess = (position: GeolocationPosition) => void;
+
+export type GeoError = (error: GeolocationPositionError) => void;
+
+const getCurrentGeolocation = (geoSuccess: GeoSuccess, geoError: GeoError) => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+  } else {
+    return `Geolocation is not supported for this Browser/OS.`;
+  }
+};
+
+export default getCurrentGeolocation;
