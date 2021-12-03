108b51978a2368c81b452ee17ef34f6aa7e1c86f
Hwajiin <borderline0px@gmail.com>
2021-12-03 17:23:29 +0900

    ⚡️ Add parameters to improve performance in makeOpenApiAxiosInstance function

--- src/service/apis/request-covid-data.ts
+++ src/service/apis/request-covid-data.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
-import generateTodayDate from "../generateDate";
-
-const date = generateTodayDate();
+import generateDate from "../generateDate";
 
 export interface AxiosConfig {
   baseURL: string;
@@ -9,18 +7,26 @@ export interface AxiosConfig {
   headers?: any;
 }
 
-// covid19 시·도발생 및 연령별·성별감염 현황에 관한 axios instance
-const openApiConfig: AxiosConfig = {
-  baseURL: "http://openapi.data.go.kr/openapi/service/rest/Covid19",
-  params: {
-    serviceKey: process.env.REACT_APP_OPEN_API_KEY,
-    pageNo: 1,
-    numOfRows: 10,
-    startCreateDt: date,
-    endCreateDt: date,
-  },
+// current date 생성
+const date = generateDate();
+
+// covid19 시·도발생 및 연령별·성별감염 현황에 관한 axios instance 만드는 함수
+const makeOpenApiAxiosInstance = (
+  startCreateDt: string = date,
+  endCreateDt: string = date
+) => {
+  const openApiConfig: AxiosConfig = {
+    baseURL: "http://openapi.data.go.kr/openapi/service/rest/Covid19",
+    params: {
+      serviceKey: process.env.REACT_APP_OPEN_API_KEY,
+      pageNo: 1,
+      numOfRows: 10,
+      startCreateDt,
+      endCreateDt,
+    },
+  };
+
+  return axios.create(openApiConfig);
 };
 
-const openApiAxiosInstance = axios.create(openApiConfig);
-
-export default openApiAxiosInstance;
+export default makeOpenApiAxiosInstance;
