05766a883684906d85c67ecf7b5a1fc2473b01b6
Hwajiin <borderline0px@gmail.com>
2021-12-03 17:16:51 +0900

    ♻️ Improve recyclability about generateDate function

--- src/service/generateDate.ts
+++ src/service/generateDate.ts
@@ -1,19 +1,22 @@
-// yyyymmdd 형식으로 변환해주기 위한 두 가지 종류의 함수
-const generateTodayDate = () => {
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-
-  return `${year}${month}${day}`;
+// 10 이하의 숫자 앞자리수에 0을 추가하는 함수
+const attachZeroToLeadingDigit = (day: string | number): string => {
+  if (typeof day === "string") {
+    return parseInt(day) < 10 ? `0${day}` : day;
+  } else {
+    return day < 10 ? `0${day}` : day.toString();
+  }
 };
 
-const altGenterateDate = () => {
-  const date = new Date().toLocaleDateString();
-  return date
+// yyyymmdd 형식으로 변환해주기 위한 함수
+const generateDate = (fewDaysAgo: number = 0) => {
+  const date = new Date();
+  date.setDate(date.getDate() - fewDaysAgo);
+  const targetDate = date.toLocaleDateString();
+
+  return targetDate
     .split(".")
-    .map((item) => item.trim())
+    .map((item) => attachZeroToLeadingDigit(item.trim()))
     .join("");
 };
 
-export default generateTodayDate;
+export default generateDate;
