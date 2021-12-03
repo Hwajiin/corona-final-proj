a0c54c041d90f83974f31485400c1c13267d77ae
Hwajiin <borderline0px@gmail.com>
2021-12-03 17:19:06 +0900

    🎨 Add geo options and edit error handling

--- src/service/get-current-pos.ts
+++ src/service/get-current-pos.ts
@@ -1,13 +1,17 @@
 // 현재 사용자 위치를 가져오는 함수
 export type GeoSuccess = (position: GeolocationPosition) => void;
-
 export type GeoError = (error: GeolocationPositionError) => void;
+
+const geoOptions = {
+  maximumAge: 5 * 60 * 1000,
+  timeout: 10 * 1000,
+};
 
 const getCurrentGeolocation = (geoSuccess: GeoSuccess, geoError: GeoError) => {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
   } else {
-    return `Geolocation is not supported for this Browser/OS.`;
+    console.log(`Geolocation is not supported for this Browser/OS.`);
   }
 };
 
