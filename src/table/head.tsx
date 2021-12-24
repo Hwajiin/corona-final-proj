01c65f226164a4351024d6ff923e1f7f34d23940
Hwajiin <borderline0px@gmail.com>
2021-12-24 17:59:00 +0900

    📱 Add responsive to font-size

--- src/table/head.tsx
+++ src/table/head.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import colors from "../base/constants/colors";
+import fonts from "../base/constants/fonts";
+import responsive from "../base/constants/responsive";
 import whitespace from "../base/constants/whitespace";
 
 interface HeadProps {
@@ -8,7 +10,7 @@ interface HeadProps {
 }
 
 const THead = styled.thead`
-  background-color: ${colors.variable["blue-light"]};
+  background-color: ${colors.variable["blue-dark"]};
   color: ${colors.variable.white};
 `;
 
@@ -18,6 +20,12 @@ const Th = styled.th`
   padding: ${whitespace.padding.sm}px 0;
   border-top: 1px solid ${colors.variable.gray};
   border-bottom: 1px solid ${colors.variable.gray};
+  font-size: ${fonts.size.sm};
+  font-weight: 500;
+
+  ${responsive.device["above-tablet"]} {
+    font-size: ${fonts.size.md};
+  }
 `;
 
 const Head: React.FC<HeadProps> = ({ headLabels }) => {
