fed3934e35652a11e5a9d7e9a1d2f3d0fb4b7c60
Hwajiin <borderline0px@gmail.com>
2021-12-24 17:54:33 +0900

    💄 Add style to section and update  font-size when on tablet

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 import fonts from "../base/constants/fonts";
+import responsive from "../base/constants/responsive";
+import whitespace from "../base/constants/whitespace";
 
 interface SectionProps {
   title: string;
 }
 
-const SSection = styled.section``;
+const SSection = styled.section`
+  width: 100%;
+  padding: 0 ${whitespace.padding.lg}px;
+  margin-bottom: 50px;
+`;
 
 const Title = styled.h2`
   font-size: ${fonts.size.md};
   margin-bottom: 5px;
+
+  ${responsive.device["above-tablet"]} {
+    font-size: ${fonts.size.lg};
+  }
 `;
 
 const Section: React.FC<SectionProps> = ({ title, children }) => {
