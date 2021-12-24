a7701e8f30df2e85ab6e28a53396240b6c256c3d
Hwajiin <borderline0px@gmail.com>
2021-12-24 17:49:48 +0900

    📈 Update color of genDefChart and add style

--- src/charts/genDefChart.tsx
+++ src/charts/genDefChart.tsx
@@ -9,9 +9,13 @@ interface GenDefChartProps {
 
 ChartJS.register(ArcElement, Title, Tooltip, Legend);
 
+const style = {
+  width: "100%",
+  height: "100%",
+};
+
 const GenDefChart: React.FC<GenDefChartProps> = ({ genCovidData }) => {
   const defPercentData = genCovidData?.map((item) => item.confCaseRate);
-  console.log(defPercentData);
 
   const data = {
     labels: ["여성", "남성"],
@@ -19,12 +23,18 @@ const GenDefChart: React.FC<GenDefChartProps> = ({ genCovidData }) => {
       {
         label: "",
         data: defPercentData,
-        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
+        backgroundColor: ["rgba(255, 23, 68, 0.3)", "rgba(41, 143, 255, 0.5)"],
       },
     ],
   };
 
-  return <Doughnut data={data} />;
+  return (
+    <Doughnut
+      data={data}
+      options={{ maintainAspectRatio: false }}
+      style={style}
+    />
+  );
 };
 
 export default GenDefChart;
