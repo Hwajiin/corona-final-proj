d39fc76d455dd639fcc41a90b3f2994d6d5be373
Hwajiin <borderline0px@gmail.com>
2021-12-28 13:15:36 +0900

    🚸 Add geoOptions to getCurrentGeolocation function

--- client/src/service/get-current-pos.ts
+++ client/src/service/get-current-pos.ts
@@ -2,9 +2,15 @@
 export type GeoSuccess = (position: GeolocationPosition) => void;
 export type GeoError = (error: GeolocationPositionError) => void;
 
+const geoOptions = {
+  enableHighAccuracy: true,
+  timeout: 50000,
+  maximumAge: 5 * 60 * 1000,
+};
+
 const getCurrentGeolocation = (geoSuccess: GeoSuccess, geoError: GeoError) => {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
   } else {
     alert(`Geolocation is not supported for this Browser/OS.`);
   }
